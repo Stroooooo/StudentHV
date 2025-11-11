866563734f55e84a164ed8ebfe9341c092799419
Struan Mclean <struanmclean271107@gmail.com>
2025-11-11 13:03:27 +0000

    updates

--- website/components/page/login-page.tsx
+++ website/components/page/login-page.tsx
@@ -4,7 +4,7 @@ export default function LoginPage() {
     const BACKGROUND = process.env.NEXT_PUBLIC_LOGIN_BACKGROUND_IMAGE
 
     return (
-        <div className={"min-w-screen min-h-screen bg-muted bg-center flex flex-col items-center justify-center"} style={{backgroundImage: BACKGROUND}}>
+        <div className={"min-w-screen min-h-screen bg-muted bg-center flex flex-col items-center justify-center"} style={{backgroundImage: `url(${BACKGROUND})`, backgroundSize: 'cover', backgroundPosition: 'center'}}>
             <div className="min-w-screen min-h-screen backdrop-blur-md flex flex-col items-center justify-center p-6 md:p-10">
                 <div className="w-full max-w-sm md:max-w-3xl ">
                     <LoginForm />
