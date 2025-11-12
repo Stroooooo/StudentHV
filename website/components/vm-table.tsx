f1da607b244eb21037e9d0ff5c40744d3ce6cdda
Struan Mclean <struanmclean271107@gmail.com>
2025-11-12 09:47:18 +0000

    changes for some reasdon

--- website/components/vm-table.tsx
+++ website/components/vm-table.tsx
@@ -16,12 +16,7 @@ import {
   getSortedRowModel,
   useReactTable,
 } from "@tanstack/react-table"
-<<<<<<< HEAD
-import { ArrowUpDown, MoreHorizontal } from "lucide-react"
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "./ui/dropdown-menu"
-=======
 import { ArrowUpDown } from "lucide-react"
->>>>>>> 0eede7c (New ability to filter)
 
 export type Vms = {
   id: string
@@ -32,11 +27,7 @@ export type Vms = {
 
 export const columns: ColumnDef<Vms>[] = [
   {
-<<<<<<< HEAD
-    accessorKey: "vmname",
-=======
     accessorKey: "Name",
->>>>>>> 0eede7c (New ability to filter)
     header: ({ column }) => {
       return (
         <Button
@@ -48,16 +39,10 @@ export const columns: ColumnDef<Vms>[] = [
         </Button>
       )
     },
-<<<<<<< HEAD
-  },
-  {
-    accessorKey: "status",
-=======
     cell: ({ row }: any) => row.original.Name.split(" ")[row.original.Name.split(" ").length - 1],
   },
   {
     accessorKey: "State",
->>>>>>> 0eede7c (New ability to filter)
     header: ({ column }) => {
       return (
         <Button
@@ -69,11 +54,6 @@ export const columns: ColumnDef<Vms>[] = [
         </Button>
       )
     },
-<<<<<<< HEAD
-  },
-  {
-    accessorKey: "cpu",
-=======
     cell: ({ row }: any) => {
       const state = row.original.State;
       if (state === 3) return "Powered Off";
@@ -87,59 +67,33 @@ export const columns: ColumnDef<Vms>[] = [
   },
   {
     accessorKey: "CPUUsage",
->>>>>>> 0eede7c (New ability to filter)
     header: ({ column }) => {
       return (
         <Button
           variant="ghost"
           onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
         >
-<<<<<<< HEAD
-            CPU
-=======
             CPU Usage
->>>>>>> 0eede7c (New ability to filter)
           <ArrowUpDown className="ml-2 h-4 w-4" />
         </Button>
       )
     },
   },
   {
-<<<<<<< HEAD
-    accessorKey: "memoryassigned",
-=======
     accessorKey: "ProcessorCount",
->>>>>>> 0eede7c (New ability to filter)
     header: ({ column }) => {
       return (
         <Button
           variant="ghost"
           onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
         >
-<<<<<<< HEAD
-            Memory Assigned
-=======
             CPUs
->>>>>>> 0eede7c (New ability to filter)
           <ArrowUpDown className="ml-2 h-4 w-4" />
         </Button>
       )
     },
   },
   {
-<<<<<<< HEAD
-    id: "actions",
-    enableHiding: false,
-    cell: ({ row }) => {
-        console.log(row)
-        return (
-            <div>
-
-            </div>
-        )
-        },
-    },
-=======
     accessorKey: "MemoryAssigned",
     header: ({ column }) => {
       return (
@@ -242,7 +196,6 @@ export const columns: ColumnDef<Vms>[] = [
       )
     },
   },
->>>>>>> 0eede7c (New ability to filter)
 ]
 
 
@@ -278,10 +231,6 @@ function bytesToSize(bytes: number, decimals = 2) {
 export default function VMTable() {
     const [sorting, setSorting] = useState<SortingState>([])
     const team = window.localStorage.getItem("@TEAM")
-<<<<<<< HEAD
-    const auth: any = useContext(AuthContext)
-=======
->>>>>>> 0eede7c (New ability to filter)
     
     const {data, isLoading, isError} = useQuery({ 
         queryKey: ['vmlist'], 
@@ -298,67 +247,8 @@ export default function VMTable() {
         }
     })
 
-<<<<<<< HEAD
-    const startVMMutation = useMutation({
-        mutationFn: async (vmConfig: {
-            vmname: string
-        }) => {
-            const response = await fetch(venv.SERVER + `/vm/${team}/${vmConfig.vmname}/start`, {
-                method: "POST",
-                credentials: 'include',
-                headers: {
-                    'Content-Type': 'application/json',
-                }
-            })
-
-            if (!response.ok) {
-                throw new Error('Failed to create VM')
-            }
-
-            return response.json()
-        },
-        onSuccess: (data) => {
-            console.log("VM started successfully:", data)
-            window.location.reload()
-        },
-        onError: (error) => {
-            console.error("VM creation failed:", error)
-        }
-    })
-
-    const stopVMMutation = useMutation({
-        mutationFn: async (vmConfig: {
-            vmname: string
-        }) => {
-            const response = await fetch(venv.SERVER + `/vm/${team}/${vmConfig.vmname}/stop`, {
-                method: "POST",
-                credentials: 'include',
-                headers: {
-                    'Content-Type': 'application/json',
-                }
-            })
-
-            if (!response.ok) {
-                throw new Error('Failed to create VM')
-            }
-
-            return response.json()
-        },
-        onSuccess: (data) => {
-            console.log("VM started successfully:", data)
-            window.location.reload()
-        },
-        onError: (error) => {
-            console.error("VM creation failed:", error)
-        }
-    })
-
-    const table = useReactTable({
-        data,
-=======
     const table = useReactTable({
         data: data || [],
->>>>>>> 0eede7c (New ability to filter)
         columns,
         getCoreRowModel: getCoreRowModel(),
         getPaginationRowModel: getPaginationRowModel(),
@@ -369,10 +259,6 @@ export default function VMTable() {
         },
     })
 
-<<<<<<< HEAD
-    return (
-
-=======
     if (isLoading) {
         return (
             <div className="min-w-[200px] flex justify-center items-center gap-5 p-5">
@@ -390,7 +276,6 @@ export default function VMTable() {
     }
 
     return (
->>>>>>> 0eede7c (New ability to filter)
       <Table>
         <TableCaption>Built by <a className="underline text-blue-500" href="https://www.linkedin.com/in/struan-mclean-821aa427b/">Struan McLean</a></TableCaption>
         <TableHeader>
@@ -434,96 +319,5 @@ export default function VMTable() {
           )}
         </TableBody>
       </Table>
-<<<<<<< HEAD
-    // <Table>
-    //     <TableHeader>
-    //         <TableRow>
-    //         <TableHead className="w-[100px]">Vm Name</TableHead>
-    //         <TableHead>Status</TableHead>
-    //         <TableHead>CPU</TableHead>
-    //         <TableHead className="text-right">Memory Assigned</TableHead>
-    //         </TableRow>
-    //     </TableHeader>
-    //     <TableBody>
-    //         {
-    //         isLoading ?
-    //             <div className="min-w-[200px] flex justify-center items-center gap-5 p-5">
-    //                 <Spinner /> <h1>Could take a while (1-2 minutes)</h1>
-    //             </div>
-    //         :
-    //             data && data.map((data: any, index:  number) => {
-    //                 let parts
-    //                 let name
-
-    //                 if (auth.isAdmin == "false") {
-    //                     parts = data.Name.split(' ');
-    //                     name = parts.slice(3).join(' ');                            
-    //                 } else {
-    //                     parts = data.Name.split(' ');
-    //                     name = parts.join(" ")
-    //                 }
-
-    //                 const state = () => {
-    //                     if (data.State == 3) {
-    //                         return "Powered Off"
-    //                     } else if (data.State == 2) {
-    //                         return "Running"
-    //                     } else if (data.State == 4) {
-    //                         return "Stoping"
-    //                     } else if (data.State == 6) {
-    //                         return "Saved (offline)"
-    //                     } else if (data.State == 9) {
-    //                         return "Paused"
-    //                     } else if (data.State == 10) {
-    //                         return "Starting"
-    //                     } else {
-    //                         return data.State
-    //                     }
-    //                 }
-
-    //                 return (
-    //                     <TableRow key={index}>
-    //                     <TableCell className="font-medium">{name}</TableCell>
-    //                     <TableCell>{state()}</TableCell>
-    //                     <TableCell>{data.CPUUsage}</TableCell>
-    //                     <TableCell className="text-right">{bytesToSize(data.MemoryAssigned)}</TableCell>
-    //                     <TableCell className="text-right flex items-center justify-end gap-5">
-    //                         <VmConnect vmName={data.Name} />
-    //                         {
-    //                         data.State == 2 || data.State == 10 ?
-    //                             <Button variant="destructive" disabled={stopVMMutation.isPending} onClick={() => {
-    //                                 stopVMMutation.mutate({vmname: data.Name})
-    //                             }}>{
-    //                                 stopVMMutation.isPending ?
-    //                                     <Spinner />
-    //                                 :
-    //                                     "Stop VM"
-    //                             }</Button>
-    //                         :
-    //                             <Button disabled={startVMMutation.isPending} onClick={() => {
-    //                                 startVMMutation.mutate({vmname: data.Name})
-    //                             }}>{
-    //                                 startVMMutation.isPending ?
-    //                                     <Spinner />
-    //                                 :
-    //                                     "Start VM"
-    //                             }</Button>
-    //                         }
-    //                         <VmSettings vmName={data.Name} />
-    //                     </TableCell>
-    //                     </TableRow>
-    //                 )
-    //             })
-    //         }
-    //     </TableBody>
-    //     <TableFooter>
-    //         <TableRow>
-    //         <TableCell colSpan={3}></TableCell>
-    //         <TableCell className="text-right"></TableCell>
-    //         </TableRow>
-    //     </TableFooter>
-    // </Table>
-=======
->>>>>>> 0eede7c (New ability to filter)
     )
 }
